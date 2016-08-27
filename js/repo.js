8f81877b849b45f0cc0b4d6b940117d0601ea4f4
Michael Bishop <polishthebishop@gmail.com>
2016-08-27 12:24:12 -0700

    Final refactor complete

--- js/repo.js
+++ js/repo.js
@@ -4,18 +4,10 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'github/users/silvershotm1/repos?sort=updated&per_page=10',
-      type: 'GET',
-    }).success(function (data) {
-      console.log('loading!');
+    $.get('github/users/silvershotm1/repos?sort=updated&per_page=10', function(data) {
       repos.all = data;
-      repos.all = repos.without('fork');
-    }).error(function(x, text, error) {
-      console.log(error);
-    }).done(
-      callback()
-    );
+      callback(data);
+    });
   };
 
   repos.with = function(attr) {
